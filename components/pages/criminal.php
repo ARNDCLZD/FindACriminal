04995c65e91b0f5b602ede434d4277348bc1c517
ARNDCLZD <64208880+ARNDCLZD@users.noreply.github.com>
2022-05-14 14:12:51 +0200

    add random fraudsters

--- components/pages/criminal.php
+++ components/pages/criminal.php
@@ -6,36 +6,28 @@ if( !$db ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
 sparql_ns( "foaf","http://xmlns.com/foaf/0.1/" );
 
 $sparql = "
-prefix dbo:<http://dbpedia.org/ontology/>
-prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix category:<http://dbpedia.org/resource/Category:>
 prefix dbr:<http://dbpedia.org/resource/>
 
-SELECT * WHERE { 
-	?criminal dbo:wikiPageWikiLink dbr:Harassment.
-    ?criminal dbo:wikiPageWikiLink dbr:France.
-
-	} LIMIT 10";
+SELECT ?wikiPageWikiLink WHERE {
+        ?wikiPageWikiLink dbo:wikiPageWikiLink category:People_convicted_of_fraud
+	}";
 
 $result = sparql_query( $sparql ); 
 if( !$result ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
-
 $fields = sparql_field_array( $result );
-
-print "<p>Number of rows: ".sparql_num_rows( $result )." results.</p>";
-print "<table class='example_table'>";
-print "<tr>";
-foreach( $fields as $field )
-{
-	print "<th>$field</th>";
-}
-print "</tr>";
+$count=0;
+$rand=rand(0,sparql_num_rows($result));
+echo($rand);
 while( $row = sparql_fetch_array( $result ) )
 {
-	print "<tr>";
 	foreach( $fields as $field )
 	{
-		print "<td>$row[$field]</td>";
+		$count++;
+		if($count==$rand){
+			print str_replace("http://dbpedia.org/resource/","",$row[$field]);
+			header('Location: http://localhost/FindACriminal/index.php?fraudster='.str_replace("http://dbpedia.org/resource/","",$row[$field]));
+		}
 	}
-	print "</tr>";
 }
-print "</table>";
+
